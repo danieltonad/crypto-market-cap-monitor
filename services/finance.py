6a3e91c44d8569b9751e2ac3ac7aa03053b510e9
danieltonad <soladan2000@gmail.com>
2024-07-29 07:03:50 -0900

    issh: tickers symbols limitations

--- services/finance.py
+++ services/finance.py
@@ -2,9 +2,11 @@ import yfinance as yf
 from requests import Session
 from settings import settings
 from logger import app_log
-from pandas import read_csv, DataFrame
-import ast, threading
+from pandas import read_csv
+import io, threading
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from pytickersymbols import PyTickerSymbols
+
 
 
 MAX_VOLUME_50 = 50_000_000_000
@@ -14,44 +16,42 @@ RESULT_LOCK = threading.Lock()
 
 results = []
 
-def fetch_available_tickers(limit: bool | int = False):
+def fetch_available_symbols(limit: bool | int = False):
     try:
         with Session() as session:
             respnse = session.get(settings.YF_SYMBOLS_URL)
             respnse.raise_for_status()
             
-            
             # stream csv file
-            data = ast.literal_eval(respnse.text)
-            print(data)
-            # Convert the data to a DataFrame
-            df = DataFrame(data, columns=['ticker', 'date', 'currency_code', 'free_cash_flow', 'total_assets', 'market_cap', 'total_revenue', 'validity'])
-            # Extract all tickers
-            tickers = df['ticker'].tolist()
-            return tickers if not limit else tickers[:limit]
+            csv_data = read_csv(io.StringIO(respnse.text))
+            
+            symbols_name = "Name"
+            if symbols_name in csv_data.columns:
+                symbols: list = csv_data[symbols_name].tolist() + ['AAPL']
+                return symbols if not limit else symbols[:limit]
+            else:
+                raise ValueError(f"Unable to locate `{symbols_name}` in csv_data")
             
     except Exception as e:
         app_log(title="FETCH_SYMBOLS_ERR", msg=e)
-    
+
 
 # Fetch stock data
 def fetch_stocks_data():
-    symbols = fetch_available_tickers()
+    symbols = fetch_available_symbols()
     app_log(title="INFO", msg=f"Symbols: {len(symbols):,}")
-    
-    # multi-thread stock data details search
-    # with ThreadPoolExecutor() as executor:
-    #     futures = [executor.submit(get_highest_volume_stocks_above_market_cap, symbol) for symbol in symbols]
-    #     app_log(title="INFO", msg="Fetching data..")
+    # get_highest_volume_stocks_above_market_cap('AAPL')
+    # multi-thread stock data details
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_highest_volume_stocks_above_market_cap, symbol) for symbol in symbols]
+        app_log(title="INFO", msg="Fetching data..")
         
-    #     for future in as_completed(futures):
-    #         pass
+        for future in as_completed(futures):
+            pass
         
-    #     app_log(title="INFO", msg="completely fetched data..")
+        app_log(title="INFO", msg="completely fetched data..")
     
     return results
-        
-    # get_highest_volume_stocks_above_market_cap('AAPL')
     
     
 def get_highest_volume_stocks_above_market_cap(symbol: str):
@@ -59,13 +59,14 @@ def get_highest_volume_stocks_above_market_cap(symbol: str):
     ticker = yf.Ticker(symbol)
     market_cap = int(ticker.info.get('marketCap', 0))
     try:
+        print(f"{market_cap:,}")
         if market_cap >= MAX_VOLUME_50:
             history = ticker.history(period="max")
             highest_volume = history['Volume'].max()
             highest_volume_date = history['Volume'].idxmax().to_pydatetime().strftime("%m:%d:%Y-%H:%M:%S")
             with RESULT_LOCK:
-                result.append([symbol, highest_volume, highest_volume_date])
+                results.append([symbol, highest_volume, highest_volume_date])
         app_log(title="FETCHED", msg=f"{symbol} data")
     except Exception as e:
-        app_log(msg=f"{symbol}_SYMBOL_ERR", error=f"Error: {str(e)}")
+        app_log(title=f"{symbol}_SYMBOL_ERR", msg=f"Error: {str(e)}")
     
