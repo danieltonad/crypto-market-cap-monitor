5753dd613aff1fde72cd090c7fa82888e1ceb2b3
danieltonad <soladan2000@gmail.com>
2024-07-29 09:37:29 -0900

    gsheet on hold

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from settings import settings
+
+
+app = FastAPI(title=settings.APP_NAME)
+
+
+@app.get('/')
+async def root():
+    from services.finance import fetch_stocks_data
+    from services.gsheet import update_google_sheet
+    
+    update_google_sheet(data=['AAPL', 7421640800, '09:29:2000-00:00:00'])
+    
+    return ""
--- services/gsheet.py
+++ services/gsheet.py
@@ -7,29 +7,40 @@ from googleapiclient.errors import HttpError
 from settings import settings
 from logger import app_log
 
-SCOPES = ["https://googleapis.com/auth/spreadsheets"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 
 # Update Google Sheet
 def update_google_sheet(data):
     credentials = None
-    if os.path.exists(settings.GSHEET_PATH):
+    if os.path.exists(settings.GTOKEN_PATH):
         credentials = Credentials.from_authorized_user_file(settings.GTOKEN_PATH, SCOPES)
-    if not credentials or not credentials.valid():
+    if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(settings.GSHEET_PATH, SCOPES)
-            credentials = flow.run_local_server(port=0)
+            credentials = flow.run_local_server(port=5000)
+            print(credentials)
         with open(settings.GTOKEN_PATH, "w") as token:
             token.write(credentials.to_json())
             
     # auth checked?
     try:
         service = build("sheets", "v4", credentials=credentials)
-        sheets = service.spreadcheets()
+        sheets = service.spreadsheets()
         
         result = sheets.values().get(spreadsheetId=settings.GSHEET_ID, range="market_cap!A1:B1").execute()
+        
+        values = result.get("values", [])
+        
+        for row in values:
+            print(values)
+            
+            
+    except HttpError as err:
+        app_log(title="Http-Error", msg=str(err))
+            
     except Exception as err:
         app_log(title="Error", msg=str(err))
             
